cbd7d1d29c80990a2f1aa08b85beb4fcb7e05c0a
alan <alan@tnk.com>
2025-06-09 15:19:59 -0300

    feat: new app accounts

---
+++ accounts/admin.py
@@ -0,0 +1,19 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth import get_user_model
+
+from accounts.forms import CustomUserChangeForm, CustomUserCreationForm
+
+
+CustomUser = get_user_model()
+
+
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    list_display = (
+        "email",
+        "username",
+        "is_superuser",
+    )
