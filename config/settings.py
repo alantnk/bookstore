9f9e78c0c0b1788bdf21e40a6d5094bbbbcee113
alan <alan@tnk.com>
2025-06-09 17:27:25 -0300

    feat: new app pages

--- config/settings.py
+++ config/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     # Local
     "accounts",
+    "pages",
 ]
 
 AUTH_USER_MODEL = "accounts.CustomUser"
@@ -64,7 +65,7 @@ ROOT_URLCONF = "config.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR / "templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
