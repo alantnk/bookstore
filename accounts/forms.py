9b3a93b56a0e7111d3f12851f8c7cd044e7a46d1
alan <alan@tnk.com>
2025-06-10 17:19:33 -0300

    feat(forms): login & signup

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,13 +1,20 @@
 from django.contrib.auth import get_user_model
-from django.contrib.auth.forms import UserChangeForm, AdminUserCreationForm
+from django.contrib.auth.forms import (
+    UserChangeForm,
+    UserCreationForm,
+    AdminUserCreationForm,
+)
 
 
-class CustomUserCreationForm(AdminUserCreationForm):
+class CustomUserCreationForm(UserCreationForm):
+    usable_password = None
+
     class Meta:
         model = get_user_model()
         fields = (
             "email",
             "username",
+            "is_staff",
         )
 
 
--- config/urls.py
+++ config/urls.py
@@ -19,6 +19,11 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
+    # Django admin
     path("admin/", admin.site.urls),
+    # User control
+    path("accounts/", include("django.contrib.auth.urls")),
+    # Local apss
+    path("accounts/", include("accounts.urls")),
     path("", include("pages.urls")),
 ]
