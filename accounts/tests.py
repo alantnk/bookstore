db0d5bad71150b1095a6ca826929443c9d663e69
alan <alan@tnk.com>
2025-06-13 18:16:03 -0300

    test: django-allauth

--- accounts/tests.py
+++ accounts/tests.py
@@ -27,3 +27,19 @@ class AccountTests(TestCase):
         self.assertTrue(admin_user.is_active)
         self.assertTrue(admin_user.is_staff)
         self.assertTrue(admin_user.is_superuser)
+
+
+class SignupPageTests(TestCase):
+    # new
+    username = "newuser"
+    email = "newuser@email.com"
+
+    def setUp(self):
+        url = reverse("account_signup")
+        self.response = self.client.get(url)
+
+    def test_signup_template(self):
+        self.assertEqual(self.response.status_code, 200)
+        self.assertTemplateUsed(self.response, "account/signup.html")
+        self.assertContains(self.response, "Sign Up")
+        self.assertNotContains(self.response, "Hi there! I should not be on the page.")
