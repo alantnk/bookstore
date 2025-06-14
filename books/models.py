5fff25eaafbe1f8cf8242ab4ca8db87d615fff64
alan <alan@tnk.com>
2025-06-14 01:50:34 -0300

    feat: reviews app

--- books/models.py
+++ books/models.py
@@ -1,6 +1,9 @@
 import uuid
 from django.db import models
 from django.urls import reverse
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 
 class Book(models.Model):
@@ -14,3 +17,16 @@ class Book(models.Model):
 
     def get_absolute_url(self):
         return reverse("book_detail", args=[str(self.id)])
+
+
+class Review(models.Model):  # new
+    book = models.ForeignKey(
+        Book,
+        on_delete=models.CASCADE,
+        related_name="reviews",
+    )
+    review = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.review
